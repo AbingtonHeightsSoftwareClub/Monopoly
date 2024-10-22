8ea0592bffe6b6b8091eed9508f53001a696c6df
JackChuck1 <134446836+JackChuck1@users.noreply.github.com>
2024-10-22 16:10:16 +0000

    Change a lot of things, mainly implemented user prompts and buying with roll Look through and see if

--- monopoly.py
+++ monopoly.py
@@ -7,20 +7,57 @@ from typing import List
 
 class Game:
     def __init__(self, players):
-        self.grid = []
+        # We'll need to actually define the grid at some point, rn its just the 23 properties
+        tiles = read_data("properties.csv")
+        self.grid = tiles
+        # I made this into a dictionary so all the calls work
         self.players = players
         self.turn = 0
+        #not sure what this variable is supposed to be, doesn't the grid contain the properties?
         self.properties = {}
 
+# Both rolls dice, moves player, and prompts player if they landed on a property
     def roll(self, player):
-        roll = random.randint(1, 6) + random.randint(1, 6)
-        self.players[player.title].position += roll
-        self.players[player.title].position %= 40
+        roll1 = random.randint(1,6)
+        roll2 = random.randint(1,6)
+        self.players[player.title].position += roll1 + roll2
+        #change to 40 once we make the acual board
+        self.players[player.title].position %= 23
+        print(roll1, roll2)
+        # If not owned prompt user
+        if not self.grid[self.players[player.title].position].owned:
+            self.decision(player)
+        #need to implement rent cost here
+        if roll1 != roll2:
+            # This will let you roll again once we implement turns
+            self.turn += 1
+            self.turn %= len(self.players)
 
+# Doesn't work rn cause of my confusion with the properties variable
+# Returns whether the buy was successful
     def buy(self, player, property):
         if self.players[player.title].money >= self.properties[property.title].price:
             self.players[player.title].properties.update({property.title: self.properties[property.title]})
             self.players[player.title].money -= self.properties[property.title].price
+            print(player.title, "bought", property.title, "for", self.properties[property.title].price)
+            return True
+        print("Not Enough Money")
+        return False
+    
+# Prompts the user with whether to buy a property, returns if the property was bought
+    def decision(self, player):
+        dec = input("Buy Property?").lower()
+# Repeats until user enters "yes" or "no"
+        while(True):
+            match dec:
+#Not calling buy method cause it doesn't work
+                case "yes":
+                    #return self.buy(player, self.grid[self.players[player.title].position])
+                    return True#
+                case "no":
+                    return False
+                case _:
+                    dec = input("Enter Yes or No").lower()
 
     def to_json(self):
         out = {
@@ -55,6 +92,8 @@ class Property:
     mortgage: int
     unmortgage: int
     color: str
+    #I think this is needed to determine if you can buy a property
+    owned: bool
 
     def dict(self):
 
@@ -80,11 +119,14 @@ def read_data(in_filename: str):
     properties = []
     for title in data.index.values:
         values = [int(x) for x in data.loc[title].iloc[0: -1]]
-        properties.append(Property(title, *values, data.loc[title].iloc[-1]))
+        properties.append(Property(title, *values, data.loc[title].iloc[-1], False))
     return properties
 
-tiles = read_data("properties.csv")
-game = Game(Player("Seamus", "dog", 10, 1500, tiles))
+player1 = Player("Seamus", "dog", 10, 1500, [])
+# The players are treated as a dict so I figured they should be a dict
+game = Game({player1.title: player1})
+print(game.players)
+game.roll(player1)
 game.to_json()
 
 
